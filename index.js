9e9a319b9d8415601dd8915d173b4b4c6cbdd80c
Saadat Yaseen <saadatyaseen309@gmail.com>
2025-04-15 13:25:10 +0500

    adding static pages

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
